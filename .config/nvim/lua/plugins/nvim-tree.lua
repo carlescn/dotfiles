1d4aae70bb7ae0f68b0043bb051f613ec70ec83c
carlescn <100298997+carlescn@users.noreply.github.com>
2024-09-24 23:54:33 +0200

    feat(nvim): add plugin nvim-tree

---
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,15 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+	},
+	config = function()
+		require("nvim-tree").setup({
+			disable_netrw = true,
+			hijack_netrw = true,
+			hijack_cursor = true,
+		})
+		local api = require("nvim-tree.api")
+		vim.keymap.set("n", "<leader>b", api.tree.toggle, { desc = "Toggle NvimTree" })
+	end,
+}
